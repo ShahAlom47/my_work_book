1851a31f1cc8d36aac2cc1b484ad5c199881af70
ShahAlom47 <sahalom4729@gmail.com>
2025-12-01 23:54:31 +0600

    update  loading  componet

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,8 +2,12 @@
 
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-     Loading...
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-50">
+      {/* Spinner */}
+      <div className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin mb-4"></div>
+
+      {/* Text */}
+      <p className="text-gray-700 text-lg font-medium">Loading, please wait...</p>
     </div>
   );
 }
