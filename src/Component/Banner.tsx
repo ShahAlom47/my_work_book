268949f4a99686fbd7f0db051b842000a0124d1f
ShahAlom47 <sahalom4729@gmail.com>
2025-12-21 23:13:29 +0600

    update

--- src/Component/Banner.tsx
+++ src/Component/Banner.tsx
@@ -11,7 +11,6 @@ const Banner = () => {
 
   return (
     <section className="relative max-w-7xl mx-auto h-[490px] sm:h-[590px] md:h-[750px] lg:h-[750px] rounded-sm  overflow-hidden shadow-md flex items-center justify-center">
-      
       {/* Desktop Image - slightly brightened */}
       <Image
         src={imgFull}
@@ -26,19 +25,11 @@ const Banner = () => {
         src={imgMobile}
         alt="MyWorkBook Banner"
         fill
-        className="object-cover absolute inset-0 md:hidden filter brightness-105"
+        className="object-cover absolute   inset-0  top-10 md:hidden filter brightness-105"
         priority
       />
 
-      {/* Lighter Overlay: less dark so image stays vibrant */}
-      {/* You can change bg-black/10 -> bg-black/5 for even lighter or bg-black/20 for slightly darker */}
-      <div className="absolute inset-0 bg-black/10 md:bg-black/10"></div>
-
-      {/* Optional subtle bottom gradient to keep text readable on top/bottom */}
-      <div className="  absolute inset-x-0 bottom-2  bg-gradient-to-t from-black/15 via-transparent to-transparent pointer-events-none">
-        {/* Content */}
-      <div className="relative z-10 text-center px-4 max-w-md mx-auto  bottom-1">
-
+      <div className="h-full flex justify-center items-end z-50 py-4 text-center px-4 max-w-md mx-auto  bottom-1 w-full ">
         {!user ? (
           <div className="space-y-3 mt-20 md:mt-0">
             <p className="text-base sm:text-lg text-white drop-shadow-sm bg-black/40 inline-block px-2 rounded-md">
@@ -61,16 +52,13 @@ const Banner = () => {
 
             <Link
               href="/my-book"
-              className="inline-block bg-blue-600 text-white font-semibold px-6 py-2 sm:px-7 sm:py-2.5 rounded-xl shadow hover:bg-blue-700 transition"
+              className="inline-block bg-blue-600 text-white font-semibold px-6 py-1 sm:px-7 sm:py-2.5 rounded-sm shadow hover:bg-blue-700 transition"
             >
               📋 My List
             </Link>
           </div>
         )}
       </div>
-      </div>
-
-    
     </section>
   );
 };
